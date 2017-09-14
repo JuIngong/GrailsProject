e0d9817d54e74e9dbac7b8932ccc365eb003ba83
Florian Muller <flolaris@gmail.com>
2017-09-14 11:13:30 +0200

    ajout bootstrap

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,5 @@
+import grailsproject.UserPasswordEncoderListener
 // Place your Spring DSL code here
 beans = {
+    userPasswordEncoderListener(UserPasswordEncoderListener, ref('hibernateDatastore'))
 }
--- grails-app/init/grailsproject/BootStrap.groovy
+++ grails-app/init/grailsproject/BootStrap.groovy
@@ -3,6 +3,19 @@ package grailsproject
 class BootStrap {
 
     def init = { servletContext ->
+        if (Role.count == 0) {
+            def roleAdmin = new Role(authority: 'ROLE_ADMIN').save(flush: true, failOnError: true)
+            def roleModo = new Role(authority: 'ROLE_MODO').save(flush: true, failOnError: true)
+            def roleUser = new Role(authority: 'ROLE_USER').save(flush: true, failOnError: true)
+
+            def userInstance = new User(password: 'user', username: 'user').save(flush: true, failOnError: true)
+            def modoInstance = new User(password: 'modo', username: 'modo').save(flush: true, failOnError: true)
+            def adminInstance = new User(password: 'admin', username: 'admin').save(flush: true, failOnError: true)
+
+            UserRole.create(userInstance, roleUser, true)
+            UserRole.create(modoInstance, roleModo, true)
+            UserRole.create(adminInstance, roleAdmin, true)
+        }
     }
     def destroy = {
     }
