b62752141c7e9fc9bcfd06c1afeb6fd3fe595a9a
Florian Muller <flolaris@gmail.com>
2017-09-19 11:35:37 +0200

    edit static rules

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -4,6 +4,7 @@
 grails.plugin.springsecurity.userLookup.userDomainClassName = 'grailsproject.User'
 grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'grailsproject.UserRole'
 grails.plugin.springsecurity.authority.className = 'grailsproject.Role'
+grails.plugin.springsecurity.logout.postOnly=false
 grails.plugin.springsecurity.controllerAnnotations.staticRules = [
 	[pattern: '/',               access: ['permitAll']],
 	[pattern: '/error',          access: ['permitAll']],
@@ -15,15 +16,23 @@ grails.plugin.springsecurity.controllerAnnotations.staticRules = [
 	[pattern: '/**/css/**',      access: ['permitAll']],
 	[pattern: '/**/images/**',   access: ['permitAll']],
 	[pattern: '/**/favicon.ico', access: ['permitAll']],
-	[pattern: '/',               access: ['permitAll']],
-	[pattern: '/location/**',               access: ['permitAll']],
-	[pattern: '/poi/**',               access: ['permitAll']],
-	[pattern: '/poigrp/**',               access: ['permitAll']],
-	[pattern: '/role/**',               access: ['permitAll']],
-	[pattern: '/user/**',               access: ['permitAll']],
-	[pattern: '/image/**',               access: ['permitAll']],
-	[pattern: '/userrole/**',               access: ['permitAll']],
-	[pattern: '/**/**',               access: ['permitAll']]
+    [pattern: '/location/create',   access: ['ROLE_ADMIN', 'ROLE_MODO']],
+    [pattern: '/location/edit/**',  access: ['ROLE_ADMIN', 'ROLE_MODO']],
+    [pattern: '/location/**', 	 access: ['permitAll']],
+    [pattern: '/poi/create',     access: ['ROLE_ADMIN', 'ROLE_MODO']],
+    [pattern: '/poi/edit/**',    access: ['ROLE_ADMIN', 'ROLE_MODO']],
+    [pattern: '/poi/**',         access: ['permitAll']],
+    [pattern: '/poigrp/create',  access: ['ROLE_ADMIN', 'ROLE_MODO']],
+    [pattern: '/poigrp/edit/**', access: ['ROLE_ADMIN', 'ROLE_MODO']],
+    [pattern: '/poigrp/**',      access: ['permitAll']],
+    [pattern: '/role/**',        access: ['ROLE_ADMIN']],
+    [pattern: '/user/create',    access: ['ROLE_ADMIN']],
+    [pattern: '/user/edit/**',   access: ['ROLE_ADMIN']],
+    [pattern: '/user/**',        access: ['ROLE_ADMIN']],
+    [pattern: '/image/create',     access: ['ROLE_ADMIN', 'ROLE_MODO']],
+    [pattern: '/image/edit/**',    access: ['ROLE_ADMIN', 'ROLE_MODO']],
+    [pattern: '/image/**',       access: ['permitAll']],
+    [pattern: '/userrole/**',    access: ['ROLE_ADMIN']]
 ]
 
 grails.plugin.springsecurity.filterChain.chainMap = [
